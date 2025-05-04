b5749108b670504827ebdabfd9ff6d6f94a67a2a
SametYASIT <120249031+SametYASIT@users.noreply.github.com>
2025-05-04 19:49:31 +0300

    Update SceneDelegate

--- mobil/SceneDelegate.swift
+++ mobil/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = UIHostingController(rootView: ContentView())
+        window.rootViewController = UIHostingController(rootView: MainTabView())
         window.makeKeyAndVisible()
         self.window = window
     }
