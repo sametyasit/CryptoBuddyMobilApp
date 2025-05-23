7f701b97a7855377111dbd3fa1e0bc2ff0d3eb5e
SametYASIT <120249031+SametYASIT@users.noreply.github.com>
2025-05-23 12:27:49 +0300

    Update CoinModel.swift

--- CoinModel.swift
+++ CoinModel.swift
@@ -27,26 +27,27 @@ struct CoinResponse: Codable {
 }
 
 // Coin modeli
-struct Coin: Codable {
+struct Coin: Codable, Equatable {
     let id: String
     let symbol: String
     let name: String
     let image: String
-    let currentPrice: Double
-    let marketCap: Double?
+    var currentPrice: Double
+    var marketCap: Double?
     let marketCapRank: Int?
-    let totalVolume: Double?
-    let high24h: Double?
-    let low24h: Double?
-    let priceChange24h: Double?
-    let priceChangePercentage: Double
-    let marketCapChange24h: Double?
-    let marketCapChangePercentage24h: Double?
+    var totalVolume: Double?
+    var high24h: Double?
+    var low24h: Double?
+    var priceChange24h: Double?
+    var priceChangePercentage: Double
+    var marketCapChange24h: Double?
+    var marketCapChangePercentage24h: Double?
     let circulatingSupply: Double?
     let totalSupply: Double?
     let maxSupply: Double?
     let ath: Double?
     let athChangePercentage: Double?
+    var lastUpdated: Date?
     
     enum CodingKeys: String, CodingKey {
         case id, symbol, name, image
@@ -65,6 +66,67 @@ struct Coin: Codable {
         case maxSupply = "max_supply"
         case ath
         case athChangePercentage = "ath_change_percentage"
+        case lastUpdated = "last_updated"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        id = try container.decode(String.self, forKey: .id)
+        symbol = try container.decode(String.self, forKey: .symbol)
+        name = try container.decode(String.self, forKey: .name)
+        image = try container.decode(String.self, forKey: .image)
+        currentPrice = try container.decode(Double.self, forKey: .currentPrice)
+        marketCap = try container.decodeIfPresent(Double.self, forKey: .marketCap)
+        marketCapRank = try container.decodeIfPresent(Int.self, forKey: .marketCapRank)
+        totalVolume = try container.decodeIfPresent(Double.self, forKey: .totalVolume)
+        high24h = try container.decodeIfPresent(Double.self, forKey: .high24h)
+        low24h = try container.decodeIfPresent(Double.self, forKey: .low24h)
+        priceChange24h = try container.decodeIfPresent(Double.self, forKey: .priceChange24h)
+        priceChangePercentage = try container.decode(Double.self, forKey: .priceChangePercentage)
+        marketCapChange24h = try container.decodeIfPresent(Double.self, forKey: .marketCapChange24h)
+        marketCapChangePercentage24h = try container.decodeIfPresent(Double.self, forKey: .marketCapChangePercentage24h)
+        circulatingSupply = try container.decodeIfPresent(Double.self, forKey: .circulatingSupply)
+        totalSupply = try container.decodeIfPresent(Double.self, forKey: .totalSupply)
+        maxSupply = try container.decodeIfPresent(Double.self, forKey: .maxSupply)
+        ath = try container.decodeIfPresent(Double.self, forKey: .ath)
+        athChangePercentage = try container.decodeIfPresent(Double.self, forKey: .athChangePercentage)
+        
+        if let lastUpdatedString = try container.decodeIfPresent(String.self, forKey: .lastUpdated) {
+            let formatter = ISO8601DateFormatter()
+            lastUpdated = formatter.date(from: lastUpdatedString)
+        } else {
+            lastUpdated = nil
+        }
+    }
+    
+    // For WebSocket real-time coin creation
+    init(id: String, symbol: String, name: String, image: String, currentPrice: Double, priceChangePercentage: Double) {
+        self.id = id
+        self.symbol = symbol
+        self.name = name
+        self.image = image
+        self.currentPrice = currentPrice
+        self.priceChangePercentage = priceChangePercentage
+        self.marketCapRank = nil
+        self.marketCap = nil
+        self.totalVolume = nil
+        self.high24h = nil
+        self.low24h = nil
+        self.priceChange24h = nil
+        self.marketCapChange24h = nil
+        self.marketCapChangePercentage24h = nil
+        self.circulatingSupply = nil
+        self.totalSupply = nil
+        self.maxSupply = nil
+        self.ath = nil
+        self.athChangePercentage = nil
+        self.lastUpdated = Date()
+    }
+    
+    // Equatable implementation for comparison
+    static func == (lhs: Coin, rhs: Coin) -> Bool {
+        return lhs.id == rhs.id
     }
     
     // Fiyat değişimini formatla (ör: +5.23% veya -2.34%)
